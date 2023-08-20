7829add8a941d26ced755f555fba687cfca70bd7
Tung2709 <vuthanhtungautomation@gmail.com>
2023-08-20 14:52:23 +0700

    first commit

--- src/Components/BookingMovie.jsx
+++ src/Components/BookingMovie.jsx
@@ -10,7 +10,12 @@ const BookingMovie = () => {
 				<div className="col-8">
 					<div className='text-center'>
 						<h1>ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
-						<h2>Màn hình</h2>
+						<h2 className='mt-3'>Màn hình</h2>
+						<div className='d-flex'>
+						<p style={{width:"50%",height:50,background:"linear-gradient(135deg, transparent 35px, orange 20px)"}}></p>
+						<p style={{width:"50%",height:50,background:"linear-gradient(135deg, transparent 35px, orange 20px)",transform: "scaleX(-1)"}}></p>
+						</div>
+						
 					</div>
 					<ChairList data={data} />
 				</div>
